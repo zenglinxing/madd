fb1f27683de2110dac1f8e542174421ba4e34799
Lin-Xing Zeng <jasonphysics@outlook.com>
2025-08-02 00:41:19 +0800

    style: large number test rename test_large-number-xxx

--- large_number/test.cmake
+++ large_number/test.cmake
@@ -1,5 +1,5 @@
-add_executable(test_uint128 large_number/test/uint128.c)
-target_include_directories(test_uint128 PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
-target_link_libraries(test_uint128 PUBLIC madd)
+add_executable(test_large-number-uint128 large_number/test/uint128.c)
+target_include_directories(test_large-number-uint128 PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+target_link_libraries(test_large-number-uint128 PUBLIC madd)
 add_test(NAME LargeNumber_uint128
-         COMMAND test_uint128)
+         COMMAND test_large-number-uint128)
