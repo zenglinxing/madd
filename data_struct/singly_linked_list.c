f993ae92583ea8dd71d03c0ef4f750f8c2d3870e
Lin-Xing Zeng <jasonphysics@outlook.com>
2025-08-19 21:48:52 +0800

    fix: singly linked list, according to Deepseek R1

--- data_struct/singly_linked_list.c
+++ data_struct/singly_linked_list.c
@@ -248,26 +248,66 @@ bool Singly_Linked_List_Delete_After(Singly_Linked_List *list, Singly_Linked_Lis
     return true;
 }
 
-bool Singly_Linked_List_Find_Loop(Singly_Linked_List_Node *head, Singly_Linked_List_Node **fast_)
+bool Singly_Linked_List_Find_Loop(Singly_Linked_List_Node *head,
+                                  Singly_Linked_List_Node **loop_start,
+                                  Singly_Linked_List_Node **tail)
 {
-    if (fast_ != NULL){
-        *fast_ = NULL;
+    if (loop_start != NULL){
+        *loop_start = NULL;
+    }
+    if (tail != NULL){
+        *tail = NULL;
     }
     if (head == NULL){
         return false;
     }
     Singly_Linked_List_Node *slow=head, *fast=head;
+    bool flag_loop = false;
     while (fast && fast->next){
         slow = slow->next;
         fast = fast->next->next;
         if (slow == fast){
-            if (fast_ != NULL){
-                *fast_ = fast;
-            }
-            return true;
+            flag_loop = true;
+            break;
         }
     }
-    return false;
+
+    if (flag_loop){
+        Singly_Linked_List_Node *p1=head, *p2=fast;
+        while (p1 != p2){
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        if (loop_start != NULL){
+            *loop_start = slow;
+        }
+        if (tail != NULL){
+            if (p1 == head){
+                Singly_Linked_List_Node *p = p1;
+                while (p->next != p1){
+                    p = p->next;
+                }
+                *tail = p;
+            }else{
+                Singly_Linked_List_Node *p = head;
+                while (p->next != p1){
+                    p = p->next;
+                }
+                *tail = p;
+            }
+        }
+        return true;
+    }else{
+        if (tail != NULL){
+            *tail = (fast != NULL) ? fast : slow;
+            if (*tail && (*tail)->next){
+                while ((*tail)->next){
+                    *tail = (*tail)->next;
+                }
+            }
+        }
+        return false;
+    }
 }
 
 bool Singly_Linked_List_Has_Loop(Singly_Linked_List *list)
@@ -281,7 +321,7 @@ bool Singly_Linked_List_Has_Loop(Singly_Linked_List *list)
 
     read_lock(list);
     Singly_Linked_List_Node *lap;
-    bool res = Singly_Linked_List_Find_Loop(list->head, &lap);
+    bool res = Singly_Linked_List_Find_Loop(list->head, &lap, NULL);
     read_unlock(list);
     return res;
 }
@@ -298,7 +338,7 @@ Singly_Linked_List_Node *Singly_Linked_List_Loop_Start_Node(Singly_Linked_List *
     read_lock(list);
     Singly_Linked_List_Node *lap = NULL;
     bool flag_has_loop;
-    flag_has_loop = Singly_Linked_List_Find_Loop(list->head, &lap);
+    flag_has_loop = Singly_Linked_List_Find_Loop(list->head, &lap, NULL);
     if (!flag_has_loop){
         read_unlock(list);
         return NULL;
@@ -332,45 +372,71 @@ bool Singly_Linked_List_Unlink_Node(Singly_Linked_List_Node *prev)
     return true;
 }
 
-void Singly_Linked_List_Reverse(Singly_Linked_List *list)
+bool Singly_Linked_List_Reverse(Singly_Linked_List *list)
 {
     if (list == NULL){
         Madd_Error_Add(MADD_ERROR, L"Singly_Linked_List_Reverse: list is NULL.");
-        return;
+        return false;
     }
 
     write_lock(list);
-    if (list->head == NULL){ /* 0 node in list */
+    if (list->head == NULL || list->head == list->tail){ /* 0 or 1 node in list */
         write_unlock(list);
-        return;
+        return true;
     }
 
-    if (list->head == list->tail){ /* 1 node in list */
-        write_unlock(list);
-        return;
+    bool flag_cyclic = (list->tail->next == list->head);
+    if (flag_cyclic){
+        list->tail->next = NULL;
     }
-    Singly_Linked_List_Node *head = list->head, *tail = list->tail;
-    if (list->head->next == list->tail){ /* 2 nodes in list */
-        head->next = tail;
-        tail->next = (tail->next == head) ? head : NULL;
-        list->head = tail;
-        list->tail = head;
-        write_unlock(list);
-        return;
-    }
-    Singly_Linked_List_Node *prev=list->head, *cur=prev->next, *next=cur->next;
+
+    Singly_Linked_List_Node *prev=NULL, *curr=list->head, *next=NULL;
     /* the next of head will be processed at the end of while loop */
-    while (next != tail){
-        cur->next = prev;
-        prev = cur;
-        cur = next;
-        next = next->next;
+    while (curr != NULL){
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
-    cur->next = prev;
-    head->next = (tail->next == head) ? tail : NULL;
-    tail->next = cur;
 
-    list->head = tail;
-    list->tail = head;
+    Singly_Linked_List_Node *old_head = list->head;
+    list->head = prev;
+    list->tail = old_head;
+
+    if (flag_cyclic){
+        list->tail->next = list->head;
+    }
     write_unlock(list);
+    return true;
+}
+
+void Singly_Linked_List_Reverse_Nodes(Singly_Linked_List_Node *head)
+{
+    if (head == NULL || head->next == head || head->next == NULL) return;
+
+    Singly_Linked_List_Node *curr = head, *tail = NULL;
+    bool flag_circular = false;
+    while (curr->next){
+        if (curr->next != NULL){
+            flag_circular = true;
+            tail = curr;
+            break;
+        }
+        curr = curr->next;
+    }
+    if (flag_circular){
+        tail->next = NULL;
+    }
+
+    Singly_Linked_List_Node *prev = NULL;
+    curr = head;
+    while (curr != NULL){
+        Singly_Linked_List_Node *next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    if (flag_circular){
+        head->next = prev;
+    }
 }
