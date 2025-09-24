895d45159ccce95649793508fead6028ebded14e
Lin-Xing Zeng <jasonphysics@outlook.com>
2025-09-24 14:52:55 +0800

    fix: when no cuda compiled, Madd_N_cuda_GPU switch to warning rather than error

--- basic/cuda_base.c
+++ basic/cuda_base.c
@@ -13,7 +13,7 @@ This file is part of Math Addition, in ./basic/cuda_base.c
 
 int Madd_N_cuda_GPU(void)
 {
-    Madd_Error_Add(MADD_ERROR, L"Madd_N_cuda_GPU: you hadn't compile Madd library with CUDA enabled. Try to re-compile Madd with ENABLE_CUDA option ON.");
+    Madd_Error_Add(MADD_WARNING, L"Madd_N_cuda_GPU: you hadn't compile Madd library with CUDA enabled. Try to re-compile Madd with ENABLE_CUDA option ON.");
     return 0;
 }
 
