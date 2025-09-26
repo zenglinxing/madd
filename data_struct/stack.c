19bcf3dd654fe0fed4c587a476c8c776ba9f9976
Lin-Xing Zeng <jasonphysics@outlook.com>
2025-09-26 10:45:04 +0800

    style: stack.c add note

--- data_struct/stack.c
+++ data_struct/stack.c
@@ -1,3 +1,10 @@
+/* coding: utf-8 */
+/*
+Author: Lin-Xing Zeng
+Email:  jasonphysics@outlook.com | jasonphysics19@gmail.com
+
+This file is part of Math Addition, in ./basic/stack.c
+*/
 #include<stdlib.h>
 #include<string.h>
 #include<stdint.h>
