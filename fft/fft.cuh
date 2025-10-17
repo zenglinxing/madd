624dca8c463ccfa7355f50984a475c85074d6d87
Lin-Xing Zeng <jasonphysics@outlook.com>
2025-10-17 20:43:29 +0800

    fix: fft.cuh macro

--- fft/fft.cuh
+++ fft/fft.cuh
@@ -5,8 +5,8 @@ Email:  jasonphysics@outlook.com | jasonphysics19@gmail.com
 
 This file is part of Math Addition, in ./fft/fft.cuh
 */
-#ifndef FFT_CUH
-#define FFT_CUH
+#ifndef MADD_FFT_CUH
+#define MADD_FFT_CUH
 
 #include<stdint.h>
 #include<stdbool.h>
@@ -19,4 +19,4 @@ bool Fast_Fourier_Transform_cuda_c32(int n, Cnum32 *arr, int fft_direction);
 void Madd_cufftPlan1d_error(int ret_plan, const char *func_name);
 void Madd_cufftExec_error(int ret, const char *func_name, const char *func_exec_name);
 
-#endif /* FFT_CUH */
+#endif /* MADD_FFT_CUH */
